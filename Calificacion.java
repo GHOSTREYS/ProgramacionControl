3cded775724ca9af0a31022e716dfe70c553bbc6
GHOSTREYS <112736493+GHOSTREYS@users.noreply.github.com>
2025-02-26 20:54:19 -0600

    Update Calificacion.java

--- Calificacion.java
+++ Calificacion.java
@@ -1,10 +1,1 @@
-/*
- * realiza un programa que determine si un alumno aprobo mediante
- * una calificacion donde la calificacion aprobatoria es de 7
- */
 
-import javax.swing.JOptionPane;
-
-public class Calificacion {
-    
-}    
